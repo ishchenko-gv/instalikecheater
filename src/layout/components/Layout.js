47e7b4a614d3b1334d1b6c9c15cdbe265b6c2c99
George Ishchenko <metalkint@gmail.com>
2018-12-16 20:03:30 +0300

    dev in progress - add footer

--- src/layout/components/Layout.js
+++ src/layout/components/Layout.js
@@ -3,16 +3,21 @@ import { Container } from 'semantic-ui-react';
 
 import Topline from '../../parts/topline/components/Topline';
 import TaskWizard from '../../parts/task-wizard/TaskWizard';
+import Footer from '../../parts/footer/components/Footer';
+import cssProjectPrefix from '../../constants/css-project-prefix';
 
 const Layout = () => {
   return (
     <Container>
-      <header className='ilc-header'>
+      <header className={`${cssProjectPrefix}-header`}>
         <Topline />
       </header>
       <main>
         <TaskWizard />
       </main>
+      <footer className={`${cssProjectPrefix}-footer`}>
+        <Footer />
+      </footer>
     </Container>
   );
 };
---
+++ src/parts/footer/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const Footer = () => {
+  return (
+    <Fragment>
+      {'Logo was found on '}
+      <a href="https://www.svgrepo.com/svg/177119/instagram-social-media" rel='noopener norefferer'>
+        svgrepo
+      </a>
+    </Fragment>
+  );
+};
+
+export default Footer;

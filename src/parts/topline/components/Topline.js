32f6077a53b53de224799fa2d56cb68e66452df5
George Ishchenko <metalkint@gmail.com>
2019-01-25 00:15:11 +0300

    dev in progress - added menu to header

--- src/parts/topline/components/Topline.js
+++ src/parts/topline/components/Topline.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Menu } from 'semantic-ui-react';
 
 import logo from '../../../img/logo.svg';
 import styles from './topline.module.scss';
 
-const  Topline = () => {
+const Topline = () => {
   return (
     <header className={styles.header}>
       <Container>
         <div className={styles.content}>
-          <img className={styles.logo} src={logo} alt='instalikecheater' />
-          <h1 className={styles.title}>
-            Instalikecheater
-          </h1>
+          <Link to='/'>
+            <div className={styles.brandRow}>
+              <img className={styles.logo} src={logo} alt='instalikecheater' />
+              <h1 className={styles.title}>
+                Instalikecheater
+              </h1>
+            </div>
+          </Link>
+          <div>
+            <Menu>
+              <Menu.Menu position='right'>
+                <Menu.Item>
+                  <Link to="/registration">
+                    Зарегистрироваться
+                  </Link>
+                </Menu.Item>
+              </Menu.Menu>
+                <Menu.Item>
+                  <Link to="/auth">
+                    Войти
+                  </Link>
+                </Menu.Item>
+            </Menu>
+          </div>
         </div>
       </Container>
     </header>
